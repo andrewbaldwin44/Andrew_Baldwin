32096a5a65a083e8df2abd911416eece68f94e05
Andrew Baldwin <andrew.baldwin44@gmail.com>
2021-01-17 14:38:13 -0500

    Add site new logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/avatar.svg',
+        icon: 'src/images/logo.png',
       },
     },
   ],
