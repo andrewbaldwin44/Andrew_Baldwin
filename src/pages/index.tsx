6675625393c4a5004f7116f9832912838271c1d3
Andrew Baldwin <andrew.baldwin44@gmail.com>
2021-01-30 20:26:40 -0500

    Setup homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,11 @@ import 'styles/index.scss';
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' />
+    <div className='c-home__heading-container'>
+      <h2>Hi there, I'm</h2>
+      <h1>Andrew Baldwin</h1>
+      <h2>Full Stack Web Developer</h2>
+    </div>
   </Layout>
 );
 
