33ce087a1cbb7df483a5ef188bbc1c1843099079
Andrew Baldwin <andrew.baldwin44@gmail.com>
2021-12-29 13:21:13 -0500

    Style contact form

--- frontend/components/contact-form/contact-form.component.tsx
+++ frontend/components/contact-form/contact-form.component.tsx
@@ -1,52 +1,103 @@
 import { useState } from 'react';
-import emailjs from 'emailjs-com';
 
+import sendEmail from 'api/email';
 import SuccessMessage from 'components/contact-form/success-message.component';
+import SendMailIcon from 'assets/send-mail';
 
 export default function ContactForm() {
-  const [success, setSuccess] = useState(false);
+  const [success, setSuccess] = useState(true);
+  const [hasFormSubmitted, setHasFormSubmitted] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+
+  const disableForm = formElement => {
+    const { elements } = formElement;
+
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].readOnly = true;
+      elements[i].disabled = true;
+    }
+
+    formElement.style.opacity = 0.6;
+  };
 
   const onSubmit = e => {
     e.preventDefault();
+    disableForm(e.target);
+    setHasFormSubmitted(true);
+    // sendEmail(e.target, setSuccess, setErrorMessage);
 
-    emailjs
-      .sendForm(
-        process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID,
-        process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID,
-        e.target,
-        process.env.NEXT_PUBLIC_EMAIL_USER_ID,
-      )
-      .then(
-        () => setSuccess(true),
-        error => setErrorMessage(error.text),
-      );
+    e.target.reset();
   };
 
-  if (success) {
-    return <SuccessMessage />;
-  }
-
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor='contact-form-name'>
-        Name:
-        <input id='contact-form-name' type='text' name='name' />
-      </label>
-      <label htmlFor='contact-form-email'>
-        Email:
-        <input id='contact-form-email' type='email' name='email' />
-      </label>
-      <label htmlFor='contact-form-subject'>
-        Subject:
-        <input id='contact-form-subject' type='text' name='subject' />
-      </label>
-      <label htmlFor='contact-form-message'>
-        Message:
-        <input id='contact-form-message' type='text' name='message' />
-      </label>
-      <button type='submit'>Send</button>
-      {errorMessage && <p>{errorMessage}</p>}
-    </form>
+    <div className='border border-gray-200 shadow rounded-lg px-12 py-10 max-w-screen-sm mx-auto mt-20'>
+      {success ? (
+        <SuccessMessage />
+      ) : (
+        <>
+          <form className='flex flex-col gap-y-8' onSubmit={onSubmit}>
+            <h1 className='text-center text-2xl'>Contact Us</h1>
+            <label
+              className='text-gray-500 text-sm flex flex-col border-b'
+              htmlFor='contact-form-name'
+            >
+              Name:
+              <input
+                className='my-2 text-black-500 text-base outline-none'
+                id='contact-form-name'
+                type='text'
+                name='name'
+                required
+              />
+            </label>
+            <label
+              className='text-gray-500 text-sm flex flex-col border-b'
+              htmlFor='contact-form-email'
+            >
+              Email:
+              <input
+                className='my-2 text-black-500 text-base outline-none'
+                id='contact-form-email'
+                type='email'
+                name='email'
+                required
+              />
+            </label>
+            <label
+              className='text-gray-500 text-sm flex flex-col border-b'
+              htmlFor='contact-form-subject'
+            >
+              Subject:
+              <input
+                className='my-2 text-black-500 text-base outline-none'
+                id='contact-form-subject'
+                type='text'
+                name='subject'
+                required
+              />
+            </label>
+            <label className='text-gray-500 text-sm flex flex-col' htmlFor='contact-form-message'>
+              Message:
+              <textarea
+                className='p-3 mt-4 border border-gray-300 rounded text-black-500 text-base outline-none'
+                rows='4'
+                id='contact-form-message'
+                type='text'
+                name='message'
+                required
+              />
+            </label>
+            <button
+              className='flex items-center justify-center w-40 py-2 gap-x-6 text-red font-semibold border-2 border-red rounded mx-auto hover:bg-red hover:text-white transition-colors duration-150'
+              type='submit'
+            >
+              Send
+              <SendMailIcon className='h-6 w-6s' />
+            </button>
+            {errorMessage && <p>{errorMessage}</p>}
+          </form>
+        </>
+      )}
+    </div>
   );
 }
--- frontend/components/contact-form/success-message.component.tsx
+++ frontend/components/contact-form/success-message.component.tsx
@@ -1,6 +1,9 @@
+import SentMailIcon from 'assets/sent-mail';
+
 export default function SuccessMessage() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center'>
+      <SentMailIcon className='h-20 w-20' />
       <p>Email successfully sent!</p>
       <p>Thanks for reaching out!</p>
     </div>
